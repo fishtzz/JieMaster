19a7283861b4c11b451c89148f7956ea9e8faeca
fishtzz <fishtzz@126.com>
2018-10-16 23:54:11 +0800

    导航页面加入

--- app/src/main/java/com/szmaster/jiemaster/MainActivity.java
+++ app/src/main/java/com/szmaster/jiemaster/MainActivity.java
@@ -1,14 +1,19 @@
 package com.szmaster.jiemaster;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.szmaster.jiemaster.model.ReportModel;
 import com.szmaster.jiemaster.network.base.ApiManager;
+import com.szmaster.jiemaster.ui.ActivityFragment;
+import com.szmaster.jiemaster.ui.HomeFragment;
+import com.szmaster.jiemaster.ui.UserFragment;
 import com.szmaster.jiemaster.utils.Log;
 
 import io.reactivex.android.schedulers.AndroidSchedulers;
@@ -17,6 +22,9 @@ import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, RadioGroup.OnCheckedChangeListener {
     private RadioGroup mRadioGroup;
+    private HomeFragment homeFragment;
+    private ActivityFragment activityFragment;
+    private UserFragment userFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +38,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void initView() {
         mRadioGroup = findViewById(R.id.radio_group);
         mRadioGroup.setOnCheckedChangeListener(this);
+        if (null == homeFragment) {
+            homeFragment = new HomeFragment();
+        }
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_frame, homeFragment);
+        transaction.commit();
     }
 
     private void test() {
@@ -80,10 +94,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onCheckedChanged(RadioGroup group, int checkedId) {
         switch (checkedId) {
             case R.id.btn_home:
+                if (null == homeFragment) {
+                    homeFragment = new HomeFragment();
+                }
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_frame, homeFragment);
+                transaction.commit();
                 break;
             case R.id.btn_activity:
+                if (null == activityFragment) {
+                    activityFragment = new ActivityFragment();
+                }
+                FragmentTransaction transaction2 = getSupportFragmentManager().beginTransaction();
+                transaction2.replace(R.id.fragment_frame, activityFragment);
+                transaction2.commit();
                 break;
             case R.id.btn_user:
+                if (null == userFragment) {
+                    userFragment = new UserFragment();
+                }
+                FragmentTransaction transaction3 = getSupportFragmentManager().beginTransaction();
+                transaction3.replace(R.id.fragment_frame, userFragment);
+                transaction3.commit();
                 break;
             default:
                 break;
---
+++ app/src/main/java/com/szmaster/jiemaster/ui/ActivityFragment.java
@@ -0,0 +1,24 @@
+package com.szmaster.jiemaster.ui;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.szmaster.jiemaster.R;
+
+public class ActivityFragment extends Fragment {
+    private View root;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        if (null == root) {
+            root = inflater.inflate(R.layout.fragment_activity, container, false);
+        }
+        return root;
+    }
+}
